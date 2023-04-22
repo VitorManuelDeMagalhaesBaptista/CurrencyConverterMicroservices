d9b4f1fbf4207426f15102bd45eeda1608264d36
Vítor Baptista <106885410+VitorManuelDeMagalhaesBaptista@users.noreply.github.com>
2023-04-22 15:15:56 +0000

    Add files via upload

---
+++ currency-conversion-service/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -0,0 +1,15 @@
+package com.in28minutes.microservices.currencyconversionservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+//@FeignClient(name = "currency-exchange", url = "localhost:8000") //com o url hardcoded atraves do feign
+@FeignClient(name = "currency-exchange")//atraves do eureka. o eureka liga os links
+public interface CurrencyExchangeProxy {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion retrieveExchangeValue(
+            @PathVariable String from,
+            @PathVariable String to);
+}
